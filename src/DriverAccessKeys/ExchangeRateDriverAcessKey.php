4e7c187eda89dbff9699f4c380779e465078abe8
chandachewe10 <chewec03@gmail.com>
2024-03-09 11:38:11 +0200

    Added Invalid Driver Exception

---
+++ src/DriverAccessKeys/ExchangeRateDriverAcessKey.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Chandachewe\Currency\DriverAccessKeys;
+class ExchangeRateDriverAcessKey{
+   /**
+     * Driver Keys.
+     *
+     * @var string
+     */
+    public static $exchangerate_access_key;
+    
+}
