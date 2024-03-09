6f9c86cfdb0f01074ddc4eb9632055f274abaa50
chandachewe10 <chewec03@gmail.com>
2024-03-09 11:14:35 +0200

    Added Invalid API Key Exception

---
+++ src/Exceptions/ApiKeyNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chandachewe\Currency\Exceptions;
+
+class ApiKeyNotFoundException extends \Exception
+{
+    public function invalidApiKey()
+    {
+        //error message
+        $errorMsg = 'Whoops, this '.$this->getMessage().'key on line '.$this->getLine().' is not a valid API Key. Are you sure you have provided the API/Access Key and it is valid?';
+
+        return $errorMsg;
+    }
+}
