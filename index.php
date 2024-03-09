ea074cb791d03bc4716972b6a293ccb0c5625330
chandachewe10 <chewec03@gmail.com>
2024-03-09 14:32:10 +0200

    Add Access Key for EXCHANGERATE DRIVE by Default

--- index.php
+++ index.php
@@ -4,3 +4,6 @@
  use Chandachewe\Currency\Drivers\ExchangeRate;
  use Chandachewe\Currency\CurrencyConverted;
  use Chandachewe\Currency\CurrencyFormats;
+
+ ExchangeConverter::convert('10', 'USD', 'ZMW');
+ echo CurrencyConverted::$currency_conversion; 
